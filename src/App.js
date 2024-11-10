0168ca25027f5af55f7e0520313001f5a9331aa4
Rodrigo Rocha <rrs230780@gmail.com>
2024-11-09 21:52:17 -0500

    Agregado logo.jpg y cambios en index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Productos from './componentes/Productos';  // Corregido: ruta adecuada para el archivo Productos
-import './App.css';
+
 
 const App = () => {
   return (
